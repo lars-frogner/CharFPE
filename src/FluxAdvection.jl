32fd4285b266a6aed6c0d7907d1ef6411e2945f3
Lars Frogner <lars_frogner@hotmail.com>
2022-10-06 15:19:32 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/FluxAdvection.jl
@@ -0,0 +1,5 @@
+module FluxAdvection
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using FluxAdvection
+using Test
+
+@testset "FluxAdvection.jl" begin
+    # Write your tests here.
+end
